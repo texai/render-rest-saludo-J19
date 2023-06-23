81bf3fe5fac44337bc59008b06fb9a7cebedd865
Ernesto Anaya <ernestex@gmail.com>
2023-06-22 22:49:01 -0500

    render con BD postgree

---
+++ src/main/java/com/example/restservice/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.restservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+// import com.example.restservice.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
